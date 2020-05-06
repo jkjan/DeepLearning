42c1e1877ce3555a8f1479434d190e35b1d7261d
jkjan <jkjan@likelion.org>
2020-05-06 20:29:50 +0900

    ELU, bn, dropout, sgd, 0.01, scheduler, kaiming : 70.73

--- CIFAR10/CNN.py
+++ CIFAR10/CNN.py
@@ -7,31 +7,31 @@ class CNN(nn.Module):
         self.batch_size = batch_size
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 16, 3, padding=1),
-            nn.LeakyReLU(),
-            # nn.Dropout2d(0.2),
+            nn.ELU(),
+            nn.Dropout2d(0.2),
             nn.BatchNorm2d(16)
         )
 
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, 3, padding=1),
-            nn.LeakyReLU(),
-            # nn.Dropout2d(0.2),
+            nn.ELU(),
+            nn.Dropout2d(0.2),
             nn.BatchNorm2d(32),
             nn.MaxPool2d(2, 2),
         )
 
         self.layer3 = nn.Sequential(
             nn.Conv2d(32, 64, 3, padding=1),
-            nn.LeakyReLU(),
-            # nn.Dropout2d(0.2),
+            nn.ELU(),
+            nn.Dropout2d(0.2),
             nn.BatchNorm2d(64),
             nn.MaxPool2d(2, 2)
         )
 
         self.fc_layer = nn.Sequential(
             nn.Linear(64*8*8, 100),
-            nn.LeakyReLU(),
-            # nn.Dropout2d(0.2),
+            nn.ELU(),
+            nn.Dropout2d(0.2),
             nn.BatchNorm1d(100),
         )
 

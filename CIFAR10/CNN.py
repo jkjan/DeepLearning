f4c084f5a67d67a5dcdd8dea0d0028dbebd54d8b
jkjan <jkjan@likelion.org>
2020-05-07 21:37:18 +0900

    70.89

--- CIFAR10/CNN.py
+++ CIFAR10/CNN.py
@@ -8,14 +8,14 @@ class CNN(nn.Module):
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 16, 3, padding=1),
             nn.BatchNorm2d(16),
-            nn.ELU(),
+            nn.ReLU(),
             nn.Dropout2d(0.3),
         )
 
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, 3, padding=1),
             nn.BatchNorm2d(32),
-            nn.ELU(),
+            nn.ReLU(),
             nn.Dropout2d(0.3),
             nn.MaxPool2d(2, 2),
         )
@@ -23,7 +23,7 @@ class CNN(nn.Module):
         self.layer3 = nn.Sequential(
             nn.Conv2d(32, 64, 3, padding=1),
             nn.BatchNorm2d(64),
-            nn.ELU(),
+            nn.ReLU(),
             nn.Dropout2d(0.3),
             nn.MaxPool2d(2, 2)
         )
@@ -31,19 +31,19 @@ class CNN(nn.Module):
         self.fc_layer = nn.Sequential(
             nn.Linear(64*8*8, 100),
             nn.BatchNorm1d(100),
-            nn.ELU(),
+            nn.ReLU(),
             nn.Dropout(0.3),
         )
         self.fc = nn.Linear(100, 10)
 
-        torch.nn.init.xavier_normal_(self.fc.weight)
-        # for m in self.modules():
-        #     if isinstance(m, nn.Conv2d):
-        #         nn.init.kaiming_uniform_(m.weight.data)
-        #         m.bias.data.fill_(0)
-        #     if isinstance(m, nn.Linear):
-        #         nn.init.kaiming_uniform_(m.weight.data)
-        #         m.bias.data.fill_(0)
+        # torch.nn.init.xavier_normal_(self.fc.weight)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight.data)
+                m.bias.data.fill_(0)
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight.data)
+                m.bias.data.fill_(0)
 
     def forward(self, x):
         out = self.layer1(x)

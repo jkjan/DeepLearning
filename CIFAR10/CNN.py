8e52c550aedbb29a483f31bc20218e3a64a1fdfa
jkjan <jkjan@likelion.org>
2020-05-07 16:46:59 +0900

    71.56

--- CIFAR10/CNN.py
+++ CIFAR10/CNN.py
@@ -7,14 +7,15 @@ class CNN(nn.Module):
         self.batch_size = batch_size
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 16, 3, padding=1),
-            # nn.Dropout2d(0.2),
             nn.ELU(),
-            nn.BatchNorm2d(16),
+            nn.Dropout2d(0.3),
+            nn.BatchNorm2d(16)
         )
 
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, 3, padding=1),
             nn.ELU(),
+            nn.Dropout2d(0.3),
             nn.BatchNorm2d(32),
             nn.MaxPool2d(2, 2),
         )
@@ -22,28 +23,28 @@ class CNN(nn.Module):
         self.layer3 = nn.Sequential(
             nn.Conv2d(32, 64, 3, padding=1),
             nn.ELU(),
+            nn.Dropout2d(0.3),
             nn.BatchNorm2d(64),
-            # nn.Dropout2d(0.2),
             nn.MaxPool2d(2, 2)
         )
 
         self.fc_layer = nn.Sequential(
             nn.Linear(64*8*8, 100),
             nn.ELU(),
+            nn.Dropout(0.3),
             nn.BatchNorm1d(100),
-            # nn.Dropout(0.1),
             # nn.Softmax(dim=1)
         )
         self.fc = nn.Linear(100, 10)
 
-        torch.nn.init.xavier_normal_(self.fc.weight)
-        # for m in self.modules():
-        #     if isinstance(m, nn.Conv2d):
-        #         nn.init.kaiming_normal_(m.weight.data)
-        #         m.bias.data.fill_(0)
-        #     if isinstance(m, nn.Linear):
-        #         nn.init.kaiming_normal_(m.weight.data)
-        #         m.bias.data.fill_(0)
+        # torch.nn.init.xavier_normal_(self.fc.weight)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data)
+                m.bias.data.fill_(0)
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight.data)
+                m.bias.data.fill_(0)
 
     def forward(self, x):
         out = self.layer1(x)

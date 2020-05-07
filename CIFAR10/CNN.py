50788a9b54321bdd14ef4a9dedb5c26ef6369d11
jkjan <jkjan@likelion.org>
2020-05-07 16:25:10 +0900

    70.71

--- CIFAR10/CNN.py
+++ CIFAR10/CNN.py
@@ -8,20 +8,20 @@ class CNN(nn.Module):
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 16, 3, padding=1),
             # nn.Dropout2d(0.2),
-            nn.LeakyReLU(),
+            nn.ELU(),
             nn.BatchNorm2d(16),
         )
 
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, 3, padding=1),
-            nn.LeakyReLU(),
+            nn.ELU(),
             nn.BatchNorm2d(32),
             nn.MaxPool2d(2, 2),
         )
 
         self.layer3 = nn.Sequential(
             nn.Conv2d(32, 64, 3, padding=1),
-            nn.LeakyReLU(),
+            nn.ELU(),
             nn.BatchNorm2d(64),
             # nn.Dropout2d(0.2),
             nn.MaxPool2d(2, 2)
@@ -29,14 +29,14 @@ class CNN(nn.Module):
 
         self.fc_layer = nn.Sequential(
             nn.Linear(64*8*8, 100),
-            nn.BatchNorm1d(100),
             nn.ELU(),
+            nn.BatchNorm1d(100),
             # nn.Dropout(0.1),
             # nn.Softmax(dim=1)
         )
         self.fc = nn.Linear(100, 10)
 
-        # torch.nn.init.xavier_normal_(self.fc.weight)
+        torch.nn.init.xavier_normal_(self.fc.weight)
         # for m in self.modules():
         #     if isinstance(m, nn.Conv2d):
         #         nn.init.kaiming_normal_(m.weight.data)
